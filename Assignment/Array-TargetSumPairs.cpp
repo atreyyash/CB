112ce59d1b6d3de2ced17b9a48d8d4aa04a2a782
Yash Atrey <atreyyash@gmail.com>
2022-08-02 00:33:49 +0530

    TargetSum, ArrayInverse

--- Assignment/Array-TargetSumPairs.cpp
+++ Assignment/Array-TargetSumPairs.cpp
@@ -1,14 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printPairs(int *a, int n, int target){
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(a[i] + a[j] == target){
-                if(a[i] < a[j]){
+void printPairs(int *a, int n, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] + a[j] == target)
+            {
+                if (a[i] < a[j])
+                {
                     cout << a[i] << " and " << a[j] << endl;
                 }
-                else{
+                else
+                {
                     cout << a[j] << " and " << a[i] << endl;
                 }
             }
@@ -16,14 +22,16 @@ void printPairs(int *a, int n, int target){
     }
 }
 
-int main(){
+int main()
+{
     int a[1000];
     int n, target;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>a[i];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
     }
-    cin>>target;
+    cin >> target;
 
     printPairs(a, n, target);
 
